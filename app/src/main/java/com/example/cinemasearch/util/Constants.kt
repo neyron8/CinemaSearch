6ca5bf4fc675b2dfff9a62f78975a8c6ff53570b
Nickolay Durakov <neyron832@gmail.com>
2024-02-01 09:11:11 +0700

    Added Constants class, gitignore has been changed

---
+++ app/src/main/java/com/example/cinemasearch/util/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.cinemasearch.util
+
+object Constants {
+    const val BASE_URL ="https://kinopoiskapiunofficial.tech/api/v2.1/"
+    const val KEY="93e9a724-19f3-4e6a-bf63-42c8589f7f4b"
+}
